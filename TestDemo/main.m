c15694f60fae74b26abffcf25f8ec540d165eb26
cocan <lgy_975@126.com>
2020-07-28 17:27:12 +0800

    testDemo

---
+++ TestDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestDemo
+//
+//  Created by Cocoa on 2020/7/28.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+//    @autoreleasepool {
+//        // insert code here...
+//        NSLog(@"Hello, World!");
+//
+//    }
+    return 0;
+}
